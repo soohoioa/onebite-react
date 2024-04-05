596143f5822805055a69a8a5e7ea6c661eddf255
hyeon-i <soohoioa@gmail.com>
2024-04-05 11:08:24 +0900

    day08

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,1 +1,22 @@
-console.log("안녕 Node.js");
+/**
+const { add, sub } = require("./math");
+
+console.log(add(1,2));
+console.log(sub(1,2));
+ */
+
+//import multiply from "./math.js";
+//import { add, sub } from "./math.js" // ES모듈 시스템을 사용할 경우 확장자까지 같이 적어줘야 한다.
+import multiply, { add, sub} from "./math.js"; 
+//같은 모듈에서 가져온다면 한번에 합쳐서 사용도 가능
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+
+/**
+console.log(add(1,2));
+console.log(sub(1,2));
+console.log(multiply(1,2));
+ */
--- section03/src/math.js
+++ section03/src/math.js
@@ -1,3 +1,25 @@
-function add(a, b) {
+// 간단한 계산 기능을 모아둔 math 모듈
+
+export function add(a, b) {
   return a + b;
+};
+
+export function sub(a, b) {
+  return a - b;
+};
+
+export default function multiply(a, b) {
+  return a * b;
 }
+/** 
+// CJS (Common JS모듈 시스템)
+module.exports = {
+  add,
+  sub,
+  // value값과 key값의 이름이 동일할경우 변수 & 함수 이름 하나만 명시해줘도 알아서 작동
+}
+*/
+
+//export { add, sub }; // export 뒤에 객체를 리터럴로 생성해서 내보내고 싶은 값들을 담아준다.
+
+

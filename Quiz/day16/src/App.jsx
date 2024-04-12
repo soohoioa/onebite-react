fd02d3ee096cda83527eaa07ddb685297eee7cfc
hyeon-i <soohoioa@gmail.com>
2024-04-12 18:00:21 +0900

    day16

---
+++ Quiz/day16/src/App.jsx
@@ -0,0 +1,65 @@
+import "./App.css";
+import ContactEditor from "./components/ContactEditor";
+import ContactList from "./components/ContactList";
+import { useReducer, useRef } from "react";
+
+const mockData = [
+  {
+    id: 0,
+    name: "이정환",
+    content: "king1997@gmail.com",
+  },
+  {
+    id: 1,
+    name: "한입스튜디오",
+    content: "onbite.fe@gmail.com",
+  },
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "DELETE":
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+};
+
+function App() {
+  const [contacts, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(2);
+
+  const onCreate = (name, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        name: name,
+        content: content,
+      },
+    });
+  };
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
+  };
+
+  return (
+    <div className="App">
+      <h2>Contact List</h2>
+      <section>
+        <ContactEditor onCreate={onCreate} />
+      </section>
+      <section>
+        <ContactList contacts={contacts} onDelete={onDelete} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
